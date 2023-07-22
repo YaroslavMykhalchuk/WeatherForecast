1cfa983b8cb91fb0ebfebf9f894bcd11c8fee5fd
Yaroslav Mykhalchuk <117015567+YaroslavMykhalchuk@users.noreply.github.com>
2023-07-22 17:34:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ searchButton.addEventListener('click', () => {
 });
 
 async function getWeather(city) {
-  const apiKey = 'api_key';
+  const apiKey = process.env.API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
   try {
